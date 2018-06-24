a4034496ce303c429758ab660f533ae1a5c2b3a7
Cody Piersall <cody.piersall@gmail.com>
2018-06-24 14:38:03 -0500

    Update example to scroll through providers.

--- example.py
+++ example.py
@@ -1,8 +1,44 @@
+import itertools
+
 from vispy import app
 import vismap
+import vismap.tile_providers as tp
 
-canvas = vismap.CanvasMap(tile_provider=vismap.StamenTonerInverted(),
-                           show=True)
+
+class CircularSequeunce:
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, item):
+        return self.data[item % len(self.data)]
+
+
+_names = list(tp.providers)
+_names = CircularSequeunce(_names)
+
+
+class Canvas(vismap.CanvasMap):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.unfreeze()
+        self._provider_index = 0
+        self._update_provider()
+        self.freeze()
+
+    def on_key_press(self, e):
+        if e.key == 'Left':
+            self._provider_index -= 1
+            self._update_provider()
+        elif e.key == 'Right':
+            self._provider_index += 1
+            self._update_provider()
+
+    def _update_provider(self):
+        name = _names[self._provider_index]
+        self.tile_provider = tp.providers[name]()
+        self.title = name
+
+canvas = Canvas(show=True)
 canvas.title = 'Maaaaps'
 
 app.run()
