f5d40d6d9f7f2f6337b4da954d0bae59ab67f883
Cody Piersall <cody.piersall@gmail.com>
2018-06-24 19:56:10 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ entry_points={
 
 setuptools.setup(
     name="vismap",
-    version="0.0.2",
+    version="0.0.3",
     author="Cody Piersall",
     author_email="cody.piersall@gmail.com",
     description="Tile maps rendered with Vispy",
