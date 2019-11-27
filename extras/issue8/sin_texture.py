71ed4e16cdcbf1de59e1dc2224ebe38838e57520
Cody Piersall <cody.piersall@gmail.com>
2019-11-26 21:08:16 -0600

    Implement sin forward transformation.

--- extras/issue8/sin_texture.py
+++ extras/issue8/sin_texture.py
@@ -22,19 +22,26 @@ def to_rgb(x):
     return p1, p2, p3
 
 
-class GoodSinTransform(BaseTransform):
+class TextureSinTransform(BaseTransform):
     """
     Applies the transformation (pos.y = sin(pos.x)) using a texture
 
     """
 
     glsl_map = """
+        #define M_PI 3.1415926535897932384626433832795
         vec4 good_sin_transform(vec4 pos) {
-            pos.y = texture1D($trans, pos.x).r;
+            // need to scale pos to be between [0, 1]
+            float x;
+            x = pos.x / (2 * M_PI);
+            // center the x coordinate now
+            x = x + 1.0 / (2 * $npoints);
+            pos.y = texture1D($trans, x).r;
             return pos;
         }"""
 
     glsl_imap = """
+        #define M_PI 3.1415926535897932384626433832795
         vec4 good_sin_transform(vec4 pos) {
             pos.y = texture1D($trans_inv, pos.x).r;
             return pos;
@@ -42,7 +49,9 @@ class GoodSinTransform(BaseTransform):
 
     def __init__(self, npoints):
         super().__init__()
-        x = np.linspace(-np.pi, np.pi, npoints, dtype='float32')
+        self.npoints = npoints
+        # we're intentionally not including the endpoint
+        x = np.linspace(0, 2 * np.pi, npoints, dtype='float32', endpoint=False)
         sin_x = np.sin(x)
         data = np.array([x, sin_x])
         self._trans_tex = gloo.Texture1D(sin_x,
@@ -53,11 +62,13 @@ class GoodSinTransform(BaseTransform):
                                          interpolation='linear',
                                          internalformat='r32f',
                                          )
-        self.shader_map()['trans'] = self._trans_tex
+        shader = self.shader_map()
+        shader['trans'] = self._trans_tex
+        shader['npoints'] = float(npoints)
         self.shader_imap()['trans_inv'] = self._trans_inv
 
 
-class LameSinTransform(BaseTransform):
+class BuiltinSinTransform(BaseTransform):
     """
     Applies the transformation (pos.y = sin(pos.x)).
 
@@ -69,14 +80,14 @@ class LameSinTransform(BaseTransform):
     Isometric = False
 
     glsl_map = """
-        vec4 LameSinTransform_map(vec4 pos) {
+        vec4 BuiltinSinTransform_map(vec4 pos) {
             pos.y = sin(pos.x);
             return pos;
         }
     """
 
     glsl_imap = """
-        vec4 LameSinTransform_imap(vec4 pos) {
+        vec4 BuiltinSinTransform_imap(vec4 pos) {
              pos.y = asin(pos.x);
             return pos;
         }
@@ -88,30 +99,34 @@ class LameSinTransform(BaseTransform):
 
 def main():
     # compare CPU and GPU implementations of sin().
-    x0 = np.linspace(-np.pi, np.pi, 100000)
+    x0 = np.linspace(-2 * np.pi, 2 * np.pi, 400000)
     sin_cpu = np.sin(x0)
 
     fig = Fig()
     ax = fig[0, 0]
     # cpu data
     sin_line_cpu = LinePlot((x0, sin_cpu), color='k')
-    # gpu data
-    lame_sin_line = LinePlot((x0, x0), color='red')
-    lame_sin_line.transform = LameSinTransform()
 
-    good_sin_line = LinePlot((x0, x0), color='blue')
-    good_sin_line.transform = GoodSinTransform(1024)
+    # gpu lines
+    gpu_sin_line = LinePlot((x0, x0), color='red')
+    gpu_sin_line.transform = BuiltinSinTransform()
+
+    coarse_sin_line = LinePlot((x0, x0), color='blue')
+    coarse_sin_line.transform = TextureSinTransform(20)
+
+    fine_sin_line = LinePlot((x0, x0), color='green', symbol='o')
+    fine_sin_line.transform = TextureSinTransform(8192 * 2)
+
 
     # logic taken from PlotWidget.plot()
     ax._configure_2d()
 
     # for line in sin_line_cpu, lame_sin_line, good_sin_line:
-    for line in sin_line_cpu, good_sin_line:
+    for line in sin_line_cpu, coarse_sin_line, fine_sin_line, gpu_sin_line:
         ax.view.add(line)
         ax.visuals.append(line)
 
     ax.title.text = 'sin(x), cpu vs gpu'
-    # ax.view.camera.rect = [1.01162, 0.530485, 0.00001, 0.00005]
     ax.view.camera.rect = [-1.02740, -0.85599, 0.00001, 0.00004]
     fig.show(run=True)
 
